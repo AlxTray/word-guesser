4f988ae9b9808d29d717b628a9fb4c3c98e6e557
AlxTray <alex.ashby02@icloud.com>
2023-08-30 19:29:43 +0100

    Added 3 rounds for each list and added check if enetered word matches shown word

--- word-guesser/src/word-guesser.cpp
+++ word-guesser/src/word-guesser.cpp
@@ -9,8 +9,8 @@ int main()
 	VectorHelper wordVector;
 
 	std::cout << "Welcome to Word Guesser!" << std::endl;
-	std::cout << "You will be given a list of words and then a word from that list with letters missing, you have to guess that word." << std::endl;
-	std::cout << "You will be given 5 seconds to memorise the list." << std::endl;
+	std::cout << "You will be given a list of words and then 3 rounds of a word being shown from that list with letters missing, you have to guess that word." << std::endl;
+	std::cout << "You will be given 15 seconds to memorise the list." << std::endl;
 
 	std::cout << "Type anything to begin." << std::endl;
 	std::string answer = "";
@@ -24,8 +24,25 @@ int main()
 		std::this_thread::sleep_for(std::chrono::milliseconds(5000)); // Pause for 5 seconds
 		std::cout << std::string(30, '\n'); // Clear console
 
-		randomWord = wordVector.getRandomWord();
-		std::cout << wordVector.getCoveredWord() << std::endl;
+		int amountCorrect = 0;
+		std::string givenWord = "";
+		for (int i = 0; i < 3; i++) {
+			std::cout << "Round " << i + 1 << std::endl;
+			randomWord = wordVector.getRandomWord();
+			std::cout << wordVector.getCoveredWord() << std::endl;
+
+			std::cout << "Enter the full word." << std::endl;
+			std::cin >> givenWord;
+			if (givenWord == randomWord) {
+				amountCorrect++;
+				std::cout << "That is correct." << std::endl;
+			}
+			else {
+				std::cout << "That is incorrect." << std::endl;
+			}
+
+			std::cout << std::string(10, '\n');
+		}
 
 		std::cout << "Type 'y' to continue, or type 'exit' to stop";
 		std::cin >> answer;
